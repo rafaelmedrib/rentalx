bb8a651d7add617b567bbf04886da3d0ec2a51f6
Rafael Medeiros Ribeiro <rafaelmedrib@gmail.com>
2022-05-17 10:59:42 -0300

    :construction: creates authentication middleware

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,57 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  token: string;
+  user: {
+    name: string;
+    email: string;
+  };
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const passwordMatches = await compare(password, user.password);
+
+    if (!passwordMatches) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const token = sign({}, "b3d49cd187db33d78e336f86b86658de", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
---
+++ src/routes/authenticate.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import { AuthenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
+const authenticateUserController = new AuthenticateUserController();
+const authenticateRoutes = Router();
+authenticateRoutes.post("/sessions", authenticateUserController.handle);
+
+export { authenticateRoutes };

506361b53796aa8fceb1666a544effbf644a53bf
Rafael Medeiros Ribeiro <rafaelmedrib@gmail.com>
2022-05-31 20:42:41 -0300

    :white_check_mark: Adds create category use case unity test

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,31 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesDatabase,
+  ICreateCategoryDTO,
+} from "../ICategoriesDatabase";
+
+class CategoriesRepositoryInMemory implements ICategoriesDatabase {
+  categories: Category[] = [];
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+
+  async list(): Promise<Category[]> {
+    const all = this.categories;
+    return all;
+  }
+
+  async alreadyContains(name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
+}
+
+export { CategoriesRepositoryInMemory };

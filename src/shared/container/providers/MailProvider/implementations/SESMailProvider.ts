c5ba2d6819efec1b6ed370fb00589ee65af8bdc5
Rafael Medeiros Ribeiro <rafaelmedrib@gmail.com>
2022-09-11 18:08:13 -0300

    :construction: before container mods

---
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,44 @@
+import { SES } from "aws-sdk";
+import fs from "fs";
+import handlebars from "handlebars";
+import nodemailer, { Transporter } from "nodemailer";
+import { injectable } from "tsyringe";
+
+import { IMailProvider } from "../IMailProvider";
+
+@injectable()
+class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    this.client = nodemailer.createTransport({
+      SES: new SES({
+        apiVersion: "2010-12-01",
+        region: process.env.AWS_SES_REGION,
+      }),
+    });
+  }
+
+  async sendMail(
+    to: string,
+    subject: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    variables: any,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
+    await this.client.sendMail({
+      to,
+      subject,
+      from: "RentalX <noreply@rentalx.com.br>",
+      html: templateHTML,
+    });
+  }
+}
+
+export { SESMailProvider };

d3218e3bb76e074af048977daf9c2c6335d496b4
Rafael Medeiros Ribeiro <rafaelmedrib@gmail.com>
2022-09-19 22:01:47 -0300

    add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
@@ -17,6 +18,7 @@ const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use(cors());
 app.use(router);
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
